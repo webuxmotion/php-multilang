b5f81b7c569eb6e5bbd7cc9ea4133b66ff2894a2
Andrii Pereverziev <pereverziev.andrii@gmail.com>
2021-12-27 00:12:23 +0200

    Initial commit

---
+++ public/index.php
@@ -0,0 +1,16 @@
+<?php
+
+$query = rtrim($_SERVER['QUERY_STRING'], '/');
+
+require '../vendor/core/Router.php';
+require '../vendor/libs/functions.php';
+
+Router::add('posts/add', ['controller' => 'Posts', 'action' => 'add']);
+Router::add('posts', ['controller' => 'Posts', 'action' => 'index']);
+Router::add('', ['controller' => 'Main', 'action' => 'index']);
+
+if (Router::matchRoute($query)) {
+  debug(Router::getRoute());
+} else {
+  echo '404';
+}
